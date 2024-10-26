32a9b208d298c800eda394945e56f6d047b13f74
Cameron Shatto <cameronshatto99@gmail.com>
2024-10-26 01:26:43 -0500

    fix(dev): add storybook-dark-mode to storybook config

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     '@storybook/addon-essentials',
     '@storybook/addon-interactions',
     '@storybook/addon-styling-webpack',
-    'storybook-addon-next-router',
+    'storybook-dark-mode',
   ],
   framework: {
     name: '@storybook/nextjs',
